0290d1893b51fd62933d413ae8a2fcbd59f8f61f
mohamdshibl <medoabdo097@gmail.com>
2023-10-26 17:56:12 +0300

    setup

---
+++ lib/core/utils/utils.dart
@@ -0,0 +1,6 @@
+class AssetsImages {
+
+  static const ads = 'assets/images/ADD.png';
+  static const ads2 = 'assets/images/cmen.png';
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'features/auth/sign_in/login.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:
+      home: Login(),
     );
   }
 }
